5d7dec2d423e1d2a2a4ebba9bf5019c62db8428b
Bauzi <volodiasyd@gmail.com>
2025-07-11 23:24:50 +0300

    feat: restructure Header component for improved layout and mobile responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,46 +47,56 @@ const Header = ({onAddRecipeClick}: HeaderProps) => {
   return (
     <header ref={headerRef} className={s.header}>
       <div className={s.headerContent}>
-        <button
-          className={s.toggleNavBarButton}
-          onClick={() => console.log('Toggle sidebar')}
-        >
-          ☰
-        </button>
-        <button
-          className={s.addButton}
-          onClick={onAddRecipeClick}
-          disabled={!user}
-          title={!user ? 'Увійдіть, щоб додати рецепт' : 'Додати новий рецепт'}
-        >
-          Добавити
-        </button>
-        <h1 className={s.headerTitle}>Домашні Рецепти</h1>
-        <div className={s.authButtons}>
-          {user ? (
-            <div className={s.userInfo}>
-              <img
-                src={user.user_metadata.avatar_url || '/default-avatar.png'}
-                alt="User Avatar"
-                className={s.avatar}
-              />
-              <span className={s.userName}>
-                {user.user_metadata.full_name?.split(' ')[0] || user.email}
-              </span>
-              <button className={s.logoutBtn} onClick={handleLogout}>
-                Вийти
-              </button>
-            </div>
-          ) : (
-            <div>
-              <button
-                onClick={() => setAuthOpen(true)}
-                className={s.authButton}
-              >
-                Увійти / Зареєструватися
-              </button>
-            </div>
-          )}
+        <div className={s.headerMain}>
+          <div className={s.hamburgerMenu}>
+            {/* Hamburger menu for mobile view */}
+            <input
+              className={s.toggleNavBarButton}
+              type="checkbox"
+              id="menuCheckbox"
+              onClick={() => console.log('Toggle sidebar')}
+            />
+            <button className={s.toggleNavBarButton}>☰</button>
+          </div>
+          <h1 className={s.headerTitle}>Домашні Рецепти</h1>
+        </div>
+        <div className={s.headerSecondary}>
+          <button
+            className={s.addButton}
+            onClick={onAddRecipeClick}
+            disabled={!user}
+            title={
+              !user ? 'Увійдіть, щоб додати рецепт' : 'Додати новий рецепт'
+            }
+          >
+            Добавити
+          </button>
+          <div className={s.authButtons}>
+            {user ? (
+              <div className={s.userInfo}>
+                <img
+                  src={user.user_metadata.avatar_url || '/default-avatar.png'}
+                  alt="User Avatar"
+                  className={s.avatar}
+                />
+                <span className={s.userName}>
+                  {user.user_metadata.full_name?.split(' ')[0] || user.email}
+                </span>
+                <button className={s.logoutBtn} onClick={handleLogout}>
+                  Вийти
+                </button>
+              </div>
+            ) : (
+              <div>
+                <button
+                  onClick={() => setAuthOpen(true)}
+                  className={s.authButton}
+                >
+                  Увійти / Зареєструватися
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
       {/* Auth Modal */}
